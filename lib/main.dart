4c8103ea2f4bd779e23f667811fbd8abfa08e668
Vedant jagtap <jvedant2109@gmail.com>
2025-09-21 20:45:03 +0530

    error fixed

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(
+   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   await GoogleSignIn.instance.initialize();
